c58157a680359d8935537595eedb7fc3937c4d93
hildelio <hildelio@gmail.com>
2023-01-11 23:45:22 -0300

    revert merge e git pull main origin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
         <img src={ logo } className="App-logo" alt="logo" />
         {
           score
-            ? <p data-testid="header-score">{score}</p>
+            ? <p data-testid="header-score">{ score }</p>
             : <p data-testid="header-score">{ 0 }</p>
         }
       </header>
